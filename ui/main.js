970d4d9fabc43390bd0d4a64b5f52e493bfbd564
Lipsa-Das <lipsadas201@gmail.com>
2016-09-30 20:26:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,7 @@
-console.log('Loaded!');
-var img = document.getElementById('yo');
-var marginLeft = 0;
-function moveRight()
-{
-    marginLeft+=10;
-    img.style.marginLeft= marginLeft +'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
-};
 var counter= 0;
-var button = document.getElementById("counter");
+var button = document.getElementById('counter');
 button.onclick = function(){
     counter+=1;
-    var span = document.getElementById("count");
+    var span = document.getElementById('count');
     span.innerHTML = counter.toString();
 };
