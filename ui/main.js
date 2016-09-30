f880eb41d7c084a3ce7da24b045270752dcf3ded
Lipsa-Das <lipsadas201@gmail.com>
2016-09-30 20:12:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,3 +9,10 @@ function moveRight()
 img.onclick = function(){
     var interval = setInterval(moveRight, 50);
 };
+
+var button = document.gerElementById("counter");
+button.onclick = function(){
+    counter+=1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
+};
