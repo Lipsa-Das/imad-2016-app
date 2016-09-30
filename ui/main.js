b802882233a2625fccea76821f9bce045956118e
Lipsa-Das <lipsadas201@gmail.com>
2016-10-01 00:19:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
     counter+=1;
-    var span= document.getElementbyId('o');
+    var span= document.getElementById('o');
     span.innerHTML=counter.toString();
 };
