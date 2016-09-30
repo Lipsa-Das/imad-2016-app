9021eac7cd4b949829af5383427339b6e5dc1d7d
Lipsa-Das <lipsadas201@gmail.com>
2016-10-01 00:24:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,6 @@ button.onclick = function(){
         }
     }
     };
-    requestopen('GET', 'http://lipsa-das.imad.hasura-app.io/counter', true );
+    request.open('GET', 'http://lipsa-das.imad.hasura-app.io/counter', true );
     request.send(null);
 };
